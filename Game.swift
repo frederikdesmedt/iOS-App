2fd9bb686a67279ccd55aba1271f20d924654ec7
Frederik De Smedt <frederik.de.smedt@hotmail.com>
2015-11-18 21:52:13 +0100

    Refactoring of Game and GameScene + card generation in UI

--- Game.swift
+++ Game.swift
@@ -3,13 +3,20 @@ import Foundation
 
 struct Game {
     
-    typealias Location = (x: Int, y: Int)
+    // No typealias because of mandatory hashable implementation
+    // typealias Location = (x: Int, y: Int)
+    
+    struct Location {
+        var x: Int
+        var y: Int
+    }
     
     static let widthInCards = 4
     static let heightInCards = widthInCards // rectangle
     
     var matrix: [[Int]] = [[Int]](count: Game.widthInCards, repeatedValue: Array(count: Game.heightInCards, repeatedValue: 0))
-    private var available = [(x: Int, y: Int)]()
+    private var available = [Location]()
+    var delegate: GameDelegate?
     
     init() {
         Game.forEachLocation {
@@ -17,38 +24,47 @@ struct Game {
         }
     }
     
-    init(withRandomValueCount: Int) {
-        self.init()
-        for _ in 0..<withRandomValueCount {
-            if let randomLocation = getRandomLocation() {
-                addRandomValue(randomLocation)
-            }
-        }
-    }
-    
     mutating func addRandomValue(location: Location) {
-        matrix[location.x][location.y] = drand48() <= 0.7 ? 2 : 4
+        let cardValue = drand48() <= 0.7 ? 2 : 4
+        matrix[location.x][location.y] = cardValue
             
         // Remove all available locations pointing to the current location (should be only one)
         available = available.filter { $0.x != location.x || $0.y != location.y }
+        if let delegate = delegate {
+            delegate.newCardWasAdded(location, value: cardValue)
+        }
     }
     
-    func getRandomLocation() -> Location? {
-        if available.isEmpty {
-            return nil;
+    var randomLocation: Location? {
+        get {
+            if available.isEmpty {
+                return nil;
+            }
+            
+            var index: UInt16 = 0
+            arc4random_buf(&index, sizeof(UInt16))
+            let location = available[Int(index) % available.count]
+            return location
         }
-        
-        var index: UInt32 = 0
-        arc4random_buf(&index, sizeof(UInt32))
-        let location = available[Int(index) % available.count]
-        return location
     }
     
     static func forEachLocation(consumer: Location -> Void) {
         for x in 0..<Game.widthInCards {
             for y in 0..<Game.heightInCards {
-                consumer((x, y))
+                consumer(Location(x: x, y: y))
             }
+        }
+    }
+}
+
+func ==(left: Game.Location, right: Game.Location) -> Bool {
+    return left.x == right.x && left.y == right.y
+}
+
+extension Game.Location: Hashable {
+    var hashValue: Int { get {
+        // assuming that x and y do not go any higher than 2^16 - 1
+            return (x << 16) & y
         }
     }
 }
--- Projecten FDS/GameScene.swift
+++ Projecten FDS/GameScene.swift
@@ -10,11 +10,15 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    let game = Game(withRandomValueCount: 1)
+    var game = Game()
     
-    var cards: [[SKNode!]]!
+    var cardBackgrounds: [Game.Location: SKNode] = [:]
+    var gameCards: [Game.Location: SKNode] = [:]
     var viewWidth: CGFloat!
     var viewHeight: CGFloat!
+    
+    var cardSize: CGFloat!
+    let margin: CGFloat = 5
     
     override func didMoveToView(view: SKView) {
         scaleMode = .AspectFill
@@ -25,8 +29,9 @@ class GameScene: SKScene {
         initGestureRecognizerForDirection(.Down, action: "swipedDown:")
         
         // Fill fixed-size cards with nil
-        cards = [[SKNode!]](count: Game.widthInCards, repeatedValue: [SKNode!](count: Game.heightInCards, repeatedValue: nil))
         backgroundColor = UIColor(red: 187 / 255, green: 173 / 255, blue: 160 / 255, alpha: 1)
+        game.delegate = self
+        game.addRandomValue(game.randomLocation!)
         createBoard()
     }
     
@@ -39,30 +44,28 @@ class GameScene: SKScene {
         size = view!.bounds.size
         viewWidth = min(size.width, size.height)
         viewHeight = viewWidth
+        cardSize = min(self.viewWidth / CGFloat(Game.widthInCards) - margin * 2, self.viewHeight / CGFloat(Game.heightInCards) - margin * 2)
         
         // Padding to center board in screen
         let paddingX = min((size.height - size.width) / 2, 0)
         let paddingY = min((size.width - size.height) / 2, 0)
         
-        let margin: CGFloat = 5
+        // Reduce multi-dimensional array to one dimensional, filter out all nil values, "unwrap" the optional values
+        removeChildrenInArray(Array(cardBackgrounds.values))
         
-        let cardSize = min(self.viewWidth / CGFloat(Game.widthInCards) - margin * 2, self.viewHeight / CGFloat(Game.heightInCards) - margin * 2)
-        
-        // Fill cards with actual SKSpriteNodes and display them on the screen
+        // Display background card layout on the screen
         Game.forEachLocation {
-            let card = SKSpriteNode(color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.2), size: CGSize(width: cardSize, height: cardSize))
-            self.cards[$0.x][$0.y] = card
-            let x = self.viewWidth * CGFloat($0.x) / CGFloat(Game.widthInCards) + cardSize / 2 + margin - paddingX
-            let y = self.viewHeight * CGFloat($0.y) / CGFloat(Game.heightInCards) + cardSize / 2 + margin - paddingY
+            let card = self.createCard()
+            self.cardBackgrounds[Game.Location(x: $0.x, y: $0.y)] = card
+            let x = self.viewWidth * CGFloat($0.x) / CGFloat(Game.widthInCards) + self.cardSize / 2 + self.margin - paddingX
+            let y = self.viewHeight * CGFloat($0.y) / CGFloat(Game.heightInCards) + self.cardSize / 2 + self.margin - paddingY
             card.position = CGPoint(x: x, y: y)
-            let label = SKLabelNode(text: "0")
-            label.fontName = "AvenirNext"
-            label.fontSize = 24
-            label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
-            label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
-            card.addChild(label)
             self.addChild(card)
         }
+    }
+    
+    func createCard() -> SKSpriteNode {
+        return SKSpriteNode(color: UIColor(red: 1, green: 1, blue: 1, alpha: 0.2), size: CGSize(width: cardSize, height: cardSize))
     }
     
     func initGestureRecognizerForDirection(direction: UISwipeGestureRecognizerDirection, action: String) {
@@ -94,20 +97,60 @@ class GameScene: SKScene {
     override func update(currentTime: CFTimeInterval) {
         
     }
+}
+
+extension GameScene {
     
     func swipedLeft(sender: UISwipeGestureRecognizer) {
-        
+        if let location = game.randomLocation {
+            game.addRandomValue(location)
+        }
     }
     
     func swipedRight(sender: UISwipeGestureRecognizer) {
-        
+        if let location = game.randomLocation {
+            game.addRandomValue(location)
+        }
     }
     
     func swipedUp(sender: UISwipeGestureRecognizer) {
-        
+        if let location = game.randomLocation {
+            game.addRandomValue(location)
+        }
     }
     
     func swipedDown(sender: UISwipeGestureRecognizer) {
+        if let location = game.randomLocation {
+            game.addRandomValue(location)
+        }
+    }
+}
+
+extension GameScene: GameDelegate {
+    func newCardWasAdded(location: Game.Location, value: Int) {
+        guard let _ = cardBackgrounds[location] else {
+            createBoard()
+            return newCardWasAdded(location, value: value)
+        }
         
+        let backgroundCard = cardBackgrounds[location]!
+        
+        let card = createCard()
+        card.position = backgroundCard.position
+        let label = SKLabelNode(text: String(value))
+        label.fontName = "AvenirNext"
+        label.fontSize = 24
+        label.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
+        label.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center
+        card.addChild(label)
+        addChild(card)
+    }
+    
+    func cardsDidMerge(from: Game.Location, to: Game.Location, oldValue: Int, newValue: Int) {
+        // TODO: Implement card animation
+    }
+    
+    func game(game: Game, score: Int) {
+        // TODO: Implement game over screen and log high score
     }
 }
